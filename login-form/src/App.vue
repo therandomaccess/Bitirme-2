3a6c871157c432ea9c796a7b50b93d558e3646b9
Fatih <128058042+therandomaccess@users.noreply.github.com>
2024-03-17 01:06:14 +0300

    Login sayfası tasarlandı

---
+++ login-form/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <Login/>
+</template>
+
+<script>
+import Login from "./Components/Login.vue";
+    export default {
+        name :"App",
+        components:{
+            Login
+        }
+
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
